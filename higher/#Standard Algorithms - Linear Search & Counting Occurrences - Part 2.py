0314d5bf413e35b1e7d875dd45d580f47d878e4b
Poppy Weatherley <P.Weatherley@albynschool.org>
2025-10-31 15:36:59 +0000

    ftuftyfttyt

--- higher/#Standard Algorithms - Linear Search & Counting Occurrences - Part 2.py
+++ higher/#Standard Algorithms - Linear Search & Counting Occurrences - Part 2.py
@@ -19,14 +19,48 @@ def findCharacterPosition(oneToFind):
   while found == False and i < len(characters):
     foundPosition = foundPosition + 1
     if characters[i] == target:
-        found = True
+        found == True
     i = i + 1
 
-  return foundPosition
+  return foundPosition, found
 
 #. *************MAIN*************
 
-target = getTargetCharacter()
 characters = getCharacters()
+target = getTargetCharacter()
 foundPosition = findCharacterPosition(target)
-print(target," was found at position ",foundPosition)
+found = findCharacterPosition(target)
+if found == True:
+  print(target," was found at position ",foundPosition)
+else:
+  print(target," was not found :(")
+
+
+
+
+
+#Task 4
+def getTarget():
+  targetAnimal = input("Please enter the target animal you are searching for: ")
+
+  return targetAnimal
+
+def getCircusAnimalsList():
+  animals = ["giraffe", "chicken", "lion", "cheetah", "rat", "elephant", "snake", "chicken", "horse"]
+
+  return animals
+
+def countOccurrences(targetAnimal, animals):
+  occurrences = 0
+  for index in range(len(animals)):
+    if animals[index] == targetAnimal:
+      occurrences = occurrences + 1
+  return occurrences
+
+
+#*************  MAIN *************
+
+targetAnimal = getTarget()
+animals= getCircusAnimalsList()
+occurrences = countOccurrences(targetAnimal, animals)
+print("There were ",occurrences," of ",targetAnimal)
