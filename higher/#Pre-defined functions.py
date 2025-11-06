2a00b6adc0848125863d686158e4f0b7e6e50ec7
Poppy Weatherley <P.Weatherley@albynschool.org>
2025-11-06 09:55:49 +0000

    dsfsffgddssdgf

--- higher/#Pre-defined functions.py
+++ higher/#Pre-defined functions.py
@@ -4,13 +4,23 @@
 value = 0
 
 password = input("Please enter your password: ")
-chars = list(password)
-for index in range(len(chars)):
-    value = value + ord(chars[index])
+characters = list(password)
+for index in range(len(characters)):
+    value = value + ord(characters[index])
 remainder = value%11
 NewPassword = str(remainder) + password
 #print so I can see if the code is working
 print(NewPassword)
 
 
+print()
+
 #Task 2
+NewAnimal = ""
+
+animal = str(input("Please enter an animal: "))
+characters = list(animal)
+characters[0] = str(characters[0]).upper()
+for index in range(len(characters)):
+    NewAnimal = NewAnimal + str(characters[index])
+print(NewAnimal)
