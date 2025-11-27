f8b1096907623079a4d9648c8efe996b56ec7787
Poppy Weatherley <P.Weatherley@albynschool.org>
2025-11-27 10:08:39 +0000

    it works yipeeeeeeeeeeee

--- higher/SDD/#Temperature Checker.py
+++ higher/SDD/#Temperature Checker.py
@@ -1,30 +1,40 @@
-#Temperature Checker
+#Temperature Checker - level 2
 
-#Subroutines
+from dataclasses import dataclass
+@dataclass
+class stuff():
+    day : int = 0
+    dayTemp : int = 0
+
+TEMP = [stuff() for index in range(14)]
 
 def getTemps ():
-   Temps = []
-   for index in range(5):
-       temp = int(input("Please input the temperature for this day in Celcius: "))
-       if temp < -20 or temp > 50 :
-          temp = int(input("TEMPERATURE MUST BE BETWEEN -20C and 50C. Please input the temperature for day",index,": "))
-       Temps.append(temp)
-   return Temps
+   TEMP = [stuff() for index in range(14)]
+   for index in range(14):
+       TEMP[index].dayTemp = int(input("Please input the temperature for this day in Celcius: "))
+       while TEMP[index].dayTemp < -20 or TEMP[index].dayTemp > 50 :
+          TEMP[index].dayTemp = int(input("TEMPERATURE MUST BE BETWEEN -20C and 50C. Please input the temperature for this day: "))
+       TEMP[index].day = index + 1
+   return TEMP
 
-def displayTemps(Temps):
-   for index in range(5):
-      print("The temperature on day ",index+1," was ",Temps[index]," degrees Celcius.")
+def displayTemps(TEMP):
+   for index in range(14):
+    if index < 8 :
+        print("The temperature on day ",TEMP[index].day," was ",TEMP[index].dayTemp," degrees Celcius.")
+    else :
+       print("The temperature on day ",TEMP[index].day," was ",TEMP[index].dayTemp," degrees Celcius.")
+       print("There was a",TEMP[index].dayTemp-TEMP[index-7].dayTemp,"difference in temperature compared to this day last week.")
 
-def getAvgTemp(Temps):
+def getAvgTemp(TEMP):
    totalTemps = 0
-   for index in range(5):
-      totalTemps = totalTemps + Temps[index]
-   avgTemp = totalTemps/5
+   for index in range(14):
+      totalTemps = totalTemps + TEMP[index].dayTemp
+   avgTemp = totalTemps/14
    print("The average temperature this week was ",avgTemp," degrees Celcius.")
 
 
 #----------- Main Program --------------
 
-Temps = getTemps()
-displayTemps(Temps)
-getAvgTemp(Temps)
+TEMP = getTemps()
+displayTemps(TEMP)
+getAvgTemp(TEMP)
