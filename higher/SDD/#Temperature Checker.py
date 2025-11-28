0f38e8071692734b62b37f78bf86f9b66af2439b
Poppy Weatherley <P.Weatherley@albynschool.org>
2025-11-28 15:36:13 +0000

    fgfjghchgchgchfchgcghc

--- higher/SDD/#Temperature Checker.py
+++ higher/SDD/#Temperature Checker.py
@@ -1,29 +1,48 @@
-#Temperature Checker - level 2
+#Temperature Checker - level 3
 
 from dataclasses import dataclass
 @dataclass
 class stuff():
-    day : int = 0
+    day : float = 0
     dayTemp : int = 0
 
 TEMP = [stuff() for index in range(14)]
 
+
+#----- Subroutines ------------------
+
 def getTemps ():
    TEMP = [stuff() for index in range(14)]
    for index in range(14):
-       TEMP[index].dayTemp = int(input("Please input the temperature for this day in Celcius: "))
+       TEMP[index].dayTemp = float(input("Please input the temperature for this day in Celcius: "))
        while TEMP[index].dayTemp < -20 or TEMP[index].dayTemp > 50 :
-          TEMP[index].dayTemp = int(input("TEMPERATURE MUST BE BETWEEN -20C and 50C. Please input the temperature for this day: "))
+          TEMP[index].dayTemp = float(input("TEMPERATURE MUST BE BETWEEN -20C and 50C. Please input the temperature for this day: "))
        TEMP[index].day = index + 1
    return TEMP
 
-def displayTemps(TEMP):
+def getmin(TEMP):
+   min = TEMP[0].dayTemp
+   for index in range(14):
+      if TEMP[index].dayTemp < min :
+         min = TEMP[index].dayTemp
+   return min
+
+
+def getmax(TEMP):
+    max = TEMP[0].dayTemp
+    for index in range (14):
+      if TEMP[index].dayTemp > max :
+         max = TEMP[index].dayTemp
+    return max
+
+def displayTemps(TEMP,min,max):
    for index in range(14):
     if index < 8 :
         print("The temperature on day ",TEMP[index].day," was ",TEMP[index].dayTemp," degrees Celcius.")
     else :
        print("The temperature on day ",TEMP[index].day," was ",TEMP[index].dayTemp," degrees Celcius.")
        print("There was a",TEMP[index].dayTemp-TEMP[index-7].dayTemp,"difference in temperature compared to this day last week.")
+   print("The lowest temperature in the last 2 weeks was",min,"and the highest was",max)
 
 def getAvgTemp(TEMP):
    totalTemps = 0
@@ -31,10 +50,17 @@ def getAvgTemp(TEMP):
       totalTemps = totalTemps + TEMP[index].dayTemp
    avgTemp = totalTemps/14
    print("The average temperature this week was ",avgTemp," degrees Celcius.")
+   return avgTemp
 
+def fileWrite(avgTemp):
+   with open("higher/SDD/avgTemps.txt","w") as wfile:
+      wfile.write(avgTemp)
 
 #----------- Main Program --------------
 
 TEMP = getTemps()
-displayTemps(TEMP)
-getAvgTemp(TEMP)
+min  = getmin(TEMP)
+max = getmax(TEMP)
+displayTemps(TEMP,min,max)
+avgTemp = getAvgTemp(TEMP)
+fileWrite(avgTemp)
