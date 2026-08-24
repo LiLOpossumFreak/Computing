2c9f8a2ac8f3955a72b0a84a3c39107b4f2e4695
LiLOpossumFreak <p.weatherley@albynschool.org>
2026-08-24 16:59:50 +0000

    i am gods stupidest soldier

--- Advanced Higher/SDD/2D_Arrays/Task4.py
+++ Advanced Higher/SDD/2D_Arrays/Task4.py
@@ -6,6 +6,7 @@ board = [
  
 for row in range(3):
 	print(board[row])
+print()
  
 winner = ''
  
@@ -17,7 +18,19 @@ for row in range(3):
 		winner = 'O'
 
 # TODO 2: check columns
-	
+s = 0
+l = 1
+for cols in range(3):
+	currentColumn = []
+	index = 0
+	for x in range(3):
+		currentColumn.append(board[index][s:l])
+		index = index + 1
+	s = s + 1
+	l = l + 1
+	#if currentColumn[0] == currentColumn[1] == currentColumn[2]:
+		#winner = currentColumn[0]
+	print(currentColumn[0])
 
 # TODO 3: check diagonals
  
