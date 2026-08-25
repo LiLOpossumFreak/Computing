709795d4b66366deaa689cb74d653e6e0f1228c2
LiLOpossumFreak <p.weatherley@albynschool.org>
2026-08-25 09:02:13 +0000

    IT WORKS!!!!!!!!!!!!!!!!!!!!!!!

--- Advanced Higher/SDD/2D_Arrays/Task4.py
+++ Advanced Higher/SDD/2D_Arrays/Task4.py
@@ -1,6 +1,6 @@
 board = [
 	['X', 'X', 'X'],
-	['O', 'X', 'O'],
+	['O', 'O', 'O'],
 	['O', 'O', 'X']
 ]
  
@@ -9,14 +9,16 @@ for row in range(3):
 print()
  
 winner = ''
-
+winningLine = []
  
 # TODO 1: check rows
 for row in range(3):
 	if board[row] == ['X', 'X', 'X']:
 		winner = 'X'
+		winningLine = 'row ' + str(row)
 	if board[row] == ['O', 'O', 'O']:
 		winner = 'O'
+		winningLine = 'row ' + str(row)
 
 
 # TODO 2: check columns
@@ -31,6 +33,7 @@ for rows in range(3):
 	s = s + 1
 	l = l + 1
 	if currentColumn[0] == currentColumn[1] == currentColumn[2]:
+		winningLine = 'column ' + str(index-1)
 		#there has to be a better solution but this works
 		#(previously printed: ['X'] has won when set winner to currentColumn[0])
 		if currentColumn[0] == ['X']:
@@ -45,25 +48,26 @@ b = 1
 for diagonals in range(2):
 	currentDiagonal = []
 	index = 0
-	for cols in range(3):
+	for squares in range(3):
 		currentDiagonal.append(board[index][s:b])
-		if diagonals == 0:
+		if diagonals == 0 and index < 2:
 			s = s + 1
 			b = b + 1
-		else:
+		elif diagonals == 1:
 			s = s - 1
 			b = b - 1
 		index = index + 1
 	if currentDiagonal[0] == currentDiagonal[1] == currentDiagonal[2]:
 		if currentDiagonal[0] == ['X']:
 			winner = 'X'
+			winningLine = 'a diagonal'
 		else:
 			winner = 'O'
-	print(currentDiagonal)
 
 
 # TODO 4: report the result
-if winner == '':
-	print('No winner')
+if winner != '':
+	print(winner, 'has won')
+	print('The win occured at',winningLine)
 else:
-    print(winner, 'has won')
+	print('No winner')
