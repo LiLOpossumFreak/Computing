b91dca39d2786500bfe67f49aad809952c6c2eb2
LiLOpossumFreak <p.weatherley@albynschool.org>
2026-08-27 14:24:39 +0000

    sad.

--- Advanced Higher/SDD/2D_Arrays/Task5.py
+++ Advanced Higher/SDD/2D_Arrays/Task5.py
@@ -10,11 +10,37 @@ seat[1][2] = 'T'
 for row in range(2):
     print(seat[row])
 
+
+class userDetails:
+	def _init_(self):
+		self.initals = ''
+		self.row = 0
+		self.column = 0
+	#setters
+	def setInitals(self, initals):
+		self.initals = initals
+	def setRow(self, row):
+		self.row = row
+	def setColumn(self, column):
+		self.column = column
+	#getters
+	def getInitals(self):
+		return self.initals
+	def getRow(self):
+		return self.row
+	def getColumn(self):
+		return self.column
+
+userDetailsVariable = userDetails()
+
 #------------------ subroutines ----------------------------------
 
 # TODO 1: ask the user for their initials, and a row and column for their seat
-def getDetailsFromUser():
-    pass
+def getDetailsFromUser(userDetailsVariable):
+	userDetailsVariable.setInitals(input("Please input your initals: "))
+	userDetailsVariable.getRow(input("Please input your row: "))
+	userDetailsVariable.getColumn(input("Please input your column: "))
+	return userDetailsVariable
 
 # TODO 2: check whether that row and column is free (equal to '')
 
@@ -26,4 +52,4 @@ def getDetailsFromUser():
 
 #----------------- main program ----------------------------------
 
-getDetailsFromUser()
+userDetailsVariable = getDetailsFromUser(userDetailsVariable)
