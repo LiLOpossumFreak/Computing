8a6c6a717ed9514cdb64ffbf797f9f6f3d34e101
LiLOpossumFreak <p.weatherley@albynschool.org>
2026-08-28 08:48:15 +0000

    rydjccgvhgvgvvjhgjvjvjg

--- Advanced Higher/SDD/2D_Arrays/Task6.py
+++ Advanced Higher/SDD/2D_Arrays/Task6.py
@@ -9,11 +9,33 @@ marks = [
  
 for r in range(len(marks)):
 	print(students[r], marks[r])
+print()
  
 # TODO 1: average mark per student
- 
+def avgMarksPerStudent(marks, students):
+	average = 0
+	for student in range(len(students)):
+		for index in range(len(marks[0])):
+			average = average + marks[student][index]
+		average = round(average/len(marks[0]))
+		print(students[student]+' gets an average of '+str(average)+' marks per test')
+	pass
+
 # TODO 2: average mark per test (per column)
- 
+def avgMarksPerTest(marks, students):
+	pass
+
 # TODO 3: highest mark, student and test
- 
+def highestMark(marks, students):
+	pass
+
 # TODO 4: lowest mark, student and test
+def lowestMark(marks, students):
+	pass
+
+#---------------- main program --------------------------------------------
+
+avgMarksPerStudent(marks, students)
+avgMarksPerTest(marks, students)
+highestMark(marks, students)
+lowestMark(marks, students)
