0c05250caa16d620440bd5acde4cb225ba4cd5a4
LiLOpossumFreak <p.weatherley@albynschool.org>
2026-09-01 09:42:11 +0000

    made a bit more readable (to me at least)

--- Advanced Higher/SDD/binary_search.py
+++ Advanced Higher/SDD/binary_search.py
@@ -1,33 +1,35 @@
+#---------------------- Subroutines ----------------------------------------------
 def initialise():
- searchlist = [1,3,5,7,9,11,13,17,18,19]
- print("Original list:",searchlist)
- return searchlist
+    searchlist = [1,3,5,7,9,11,13,17,18,19]
+    print("Original list:",searchlist)
+    return searchlist
 
 
 def BinarySearch(searchlist,goal):
- found = False
- startpos = 0
- endpos = len(searchlist) -1
+    found = False
+    startpos = 0
+    endpos = len(searchlist) -1
 
 
- print ("Endpos at beginning = ",endpos)
+    print ("Endpos at beginning = ",endpos)
 
 
- while (startpos <= endpos) and found == False:
-     middle = (startpos+endpos)//2 #Integer Division
-     if searchlist[middle] == goal:
-         found = True
-     elif searchlist[middle]<goal:
-         startpos = middle + 1
-     else:
-         endpos = middle -1
+    while (startpos <= endpos) and found == False:
+        middle = (startpos+endpos)//2 
+        if searchlist[middle] == goal:
+            found = True
+        elif searchlist[middle]<goal:
+            startpos = middle + 1
+        else:
+            endpos = middle -1
 
 
- if found == True:
-   print("Match has been found at position",middle)
- else:
-   print("Goal not found")
+    if found == True:
+        print("Match has been found at position",middle)
+    else:
+        print("Goal not found")
 
+#---------------------- Main Program ----------------------------------------------
 
 values = initialise()
 
