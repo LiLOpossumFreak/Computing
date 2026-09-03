4e02529b1f4ecc210f19f8d5ff397ab1491163ae
LiLOpossumFreak <p.weatherley@albynschool.org>
2026-09-03 14:21:45 +0000

    get comitted idiot

---
+++ Advanced Higher/SDD/insertion_sort.py
@@ -0,0 +1,20 @@
+myList = [3,4,9,7,1]
+
+
+for index in range (1,len(myList)):
+#store the value to be inserted into the array
+ currentvalue = myList[index]
+ position = index
+
+
+  #shift the rest of the array one to the right
+ while position > 0 and myList[position-1]>currentvalue:
+   myList[position] = myList[position-1]
+   position -= 1
+
+
+ #insert the value into the array
+ myList[position] = currentvalue
+
+
+print(myList)
