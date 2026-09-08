0c969c650200cace65970791a929da9ac836335b
LiLOpossumFreak <p.weatherley@albynschool.org>
2026-09-08 10:03:48 +0000

    nevermind i fixed it

--- Advanced Higher/SDD/Bubble_sort/letter_sort.py
+++ Advanced Higher/SDD/Bubble_sort/letter_sort.py
@@ -8,11 +8,11 @@ for outer in range (len(myList)-1,0,-1):
     #compare two adjacent values
     if ord(myList[inner]) > ord(myList[inner+1]):
       #assign one of the values to a temp variable
-      temp = ord(myList[inner])
+      temp = myList[inner]
       #overwrite one of the values
-      ord(myList[inner]) == ord(myList[inner+1])
+      myList[inner] = myList[inner+1]
       #replace with the temp value
-      ord(myList[inner+1]) == temp
+      myList[inner+1] = temp
       swaps = swaps + 1
 
 
