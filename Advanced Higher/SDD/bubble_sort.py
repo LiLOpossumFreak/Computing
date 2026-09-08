bc9c51c434a57a16dfd6b70f7a53bf9a2079b3ab
LiLOpossumFreak <p.weatherley@albynschool.org>
2026-09-08 09:49:37 +0000

    Tasks 1&2 done

--- Advanced Higher/SDD/bubble_sort.py
+++ Advanced Higher/SDD/bubble_sort.py
@@ -1,17 +1,21 @@
 myList = [3,4,9,7,1]
+swaps = 0
 
-
+#Sorts into descedning order
 #start from the right
 for outer in range (len(myList)-1,0,-1):
- for inner in range(outer):
-   #compare two adjacent values
-   if myList[inner]>myList[inner+1]:
-     #assign one of the values to a temp variable
-     temp = myList[inner]
-     #overwrite one of the values
-     myList[inner] = myList[inner+1]
-     #replace with the temp value
-     myList[inner+1] = temp
-  
+  for inner in range(outer):
+    #compare two adjacent values
+    if myList[inner+1]>myList[inner]:
+      #assign one of the values to a temp variable
+      temp = myList[inner+1]
+      #overwrite one of the values
+      myList[inner+1] = myList[inner]
+      #replace with the temp value
+      myList[inner] = temp
+      swaps = swaps + 1
+
+
 print("Bubble sort complete")
 print(myList)
+print("There were "+str(swaps)+" swaps")
