f0bd9b2085bc22a8ff71aa9fda94a6c89b1e75bd
LiLOpossumFreak <p.weatherley@albynschool.org>
2026-09-09 07:29:32 +0000

    nojjnjnojkn kjnknj

--- Advanced Higher/SDD/Bubble_sort/card_sort.py
+++ Advanced Higher/SDD/Bubble_sort/card_sort.py
@@ -5,15 +5,19 @@ swaps = 0
 #start from the right
 for outer in range (len(myList)-1,0,-1):
   for inner in range(outer):
-    #compare two adjacent values
-    if myList[inner]>myList[inner+1]:
-      #assign one of the values to a temp variable
-      temp = myList[inner]
-      #overwrite one of the values
-      myList[inner] = myList[inner+1]
-      #replace with the temp value
-      myList[inner+1] = temp
-      swaps = swaps + 1
+    if len(myList[inner][0:'_']) > 1:
+      #do some stuff ig?
+      print()
+    else: 
+      #compare two adjacent values
+      if int(myList[inner][0:1]) > int(myList[inner+1][0:'_']):
+         #assign one of the values to a temp variable
+        temp = myList[inner]
+         #overwrite one of the values
+        myList[inner] = myList[inner+1]
+         #replace with the temp value
+        myList[inner+1] = temp
+        swaps = swaps + 1
 
 
 print("Bubble sort complete")
