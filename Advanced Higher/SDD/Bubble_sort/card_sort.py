5a65ffe63e2cbd11e15ce870a4a204f2e1860147
LiLOpossumFreak <p.weatherley@albynschool.org>
2026-09-09 09:57:39 +0000

    bigggggggggg sad

--- Advanced Higher/SDD/Bubble_sort/card_sort.py
+++ Advanced Higher/SDD/Bubble_sort/card_sort.py
@@ -1,16 +1,17 @@
-myList = ['4_ofClubs','queen_ofSpades','3_ofDiamonds','7_ofSpades','ace_ofClubs']
+myList = ['4_ofClubs','Q_ofSpades','3_ofDiamonds','7_ofSpades','A_ofClubs']
 swaps = 0
 
 #Sorts into ascending order
 #start from the right
 for outer in range (len(myList)-1,0,-1):
   for inner in range(outer):
-    if len(myList[inner][0:'_']) > 1:
+    #checks whether starting character is a number or letter
+    if ord(myList[inner][0:1]) > 58:
       #do some stuff ig?
       print()
     else: 
-      #compare two adjacent values
-      if int(myList[inner][0:1]) > int(myList[inner+1][0:'_']):
+      #compare two adjacent integer values
+      if int(myList[inner][0:1]) > int(myList[inner+1][0:1]):
          #assign one of the values to a temp variable
         temp = myList[inner]
          #overwrite one of the values
@@ -18,7 +19,6 @@ for outer in range (len(myList)-1,0,-1):
          #replace with the temp value
         myList[inner+1] = temp
         swaps = swaps + 1
-
 
 print("Bubble sort complete")
 print(myList)
