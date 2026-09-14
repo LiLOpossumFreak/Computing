6c1d79b188378d4c4e1374b4d08533b756f591df
LiLOpossumFreak <p.weatherley@albynschool.org>
2026-09-14 09:04:20 +0000

    i hate this program bro

--- Advanced Higher/SDD/Bubble_sort/card_sort.py
+++ Advanced Higher/SDD/Bubble_sort/card_sort.py
@@ -6,18 +6,17 @@ swaps = 0
 for outer in range (len(myList)-1,0,-1):
   for inner in range(outer):
     #checks whether starting character is a number or letter
-    if ord(myList[inner][0:1]) > 40:
-      valueHold = myList[inner]
+    if ord(myList[inner+1][0:1]) > 40:
+      valueHold = myList[inner+1]
       #this is probably super inefficient but idk how to do it better and this is already late :(
-      if myList[inner][0:1] == 'A':
-          myList[inner] = 0
-      elif myList[inner][0:1] == 'J':
-        myList[inner] = 10
-      elif myList[inner][0:1] == 'Q':
-        myList[inner] = 11
-        print(myList[inner].rstrip('_'))
-      elif myList[inner][0:1] == 'K':
-        myList[inner] = 12
+      if myList[inner+1][0:1] == 'A':
+          myList[inner+1] = 0
+      elif myList[inner+1][0:1] == 'J':
+        myList[inner+1] = 10
+      elif myList[inner+1][0:1] == 'Q':
+        myList[inner+1] = 11
+      elif myList[inner+1][0:1] == 'K':
+        myList[inner+1] = 12
   
     #compare two adjacent integer values
     if int(myList[inner][0:2].rstrip('_')) > int(myList[inner+1][0:2].rstrip('_')):
